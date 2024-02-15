05e8676585341bae18340243845e1db954bb6b97
Md Abdullah Al Masum <masum06aiub@gmail.com>
2024-02-15 07:52:59 +0600

    First Commit

--- cSharpBasicToAdvance/Program.cs
+++ cSharpBasicToAdvance/Program.cs
@@ -8,7 +8,11 @@ namespace cSharpBasicToAdvance
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("");
+            Console.WriteLine("Write Your Name:");
+            string userName = Console.ReadLine();
+            string Verbatim = @"Verbatim Literal is a string with an @ symbol prefix, as in @“Hello”. Verbatim literals make escape sequences translate as normal printable characters to enhance readability.";
+            Console.WriteLine("My Name is " + userName);
+            Console.WriteLine(Verbatim);
         }
     }
 }
