35c195c8bd6499802d46c25586e71622e66c09bd
Md Abdullah Al Masum <52232643+masum029@users.noreply.github.com>
2024-02-20 18:22:37 +0600

    duplicate char

---
+++ ConsoleAppB2A/Interview/RemoveDuplicateCharactersFromString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppB2A.Interview
+{
+    public class RemoveDuplicateCharactersFromString
+    {
+        static string RemoveDuplicateChars(string key)
+        {
+            // --- Removes duplicate chars using string concats. ---
+            // Store encountered letters in this string.
+            string table = "";
+
+            // Store the result in this string.
+            string result = "";
+
+            // Loop over each character.
+            foreach (char value in key)
+            {
+                // See if character is in the table.
+                if (table.IndexOf(value) == -1)
+                {
+                    // Append to the table and the result.
+                    table += value;
+                    result += value;
+                }
+            }
+            return result;
+        }
+    }
+}
